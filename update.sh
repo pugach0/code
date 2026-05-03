055ef6a56aecc04b3f39c88acd8a5c451337144f
pugach0 <blissfuldreams0@gmail.com>
2026-05-03 22:18:12 +0300

    update fix thing

--- update.sh
+++ update.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 config="$HOME/.config/study_tracker/"
-cd $HOME
-rm -rf "$HOME/study_tracker/"
-git clone "https://github.com/pugach0/study_tracker.git"
-cd "study_tracker"
+repo="$HOME/study_tracker"
+
+rm -rf "$repo"
+git clone "https://github.com/pugach0/study_tracker.git" "$repo"
 mkdir -p "$config"
-cp -r $PWD/* "$config"
-cp $PWD/study_tracker.sh "$HOME/.local/bin/"
-cp $PWD/toggl-time.sh "$HOME/.local/bin/"
+cp -r "$repo"/* "$config"
+cp "$repo/study_tracker.sh" "$HOME/.local/bin/"
+cp "$repo/toggl-time.sh" "$HOME/.local/bin/"
 echo "update successful"
